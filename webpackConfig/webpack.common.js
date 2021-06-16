0315022d21894623b7ed46f63d38f6ca11bc67ba
Gonzalo Piffaretti <gpiffaretti@fsstudio.com>
2021-06-16 18:22:14 -0300

    Created webpack configs for dev and prod. Added ifdef loader to add preprocessor directives for conditional compilation.

---
+++ webpackConfig/webpack.common.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const ifdefLoaderOptions = {
+    DEBUG: true,
+    LOGGING: true,
+    version: 3,
+    "ifdef-verbose": true,                 // add this for verbose output
+    "ifdef-triple-slash": true,            // add this to use double slash comment instead of default triple slash
+    "ifdef-fill-with-blanks": true,        // add this to remove code with blank spaces instead of "//" comments
+    "ifdef-uncomment-prefix": "// #code "  // add this to uncomment code starting with "// #code "
+ };
+
+const contextPath = path.resolve(__dirname, "../");
+console.log("webpack context: " + contextPath);
+
+module.exports = (env) => {
+    return {
+        context: path.resolve(__dirname, "../"),
+        entry: "./app/src/testApp.js",
+        module: {
+            rules: [
+                
+            ]
+        },
+        optimization: {
+            minimize: true,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'ThreeJS tests',
+                template: './index.html',
+            }),
+        ],
+        devServer: {
+            contentBase: './dist',
+        },
+        watchOptions: {
+            aggregateTimeout: 500,
+            ignored: ['**/node_modules'],
+        },
+        output: {
+            filename: "main.js",
+            path: path.join(contextPath, 'dist'),
+            clean: true,
+        },
+    };
+}
---
+++ webpackConfig/webpack.dev.js
@@ -0,0 +1,39 @@
+const { mergeWithRules } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+const ifdefLoaderOptions = {
+    LOGGING: true,
+    "ifdef-verbose": true,                 // add this for verbose output
+    "ifdef-triple-slash": true,            // add this to use double slash comment instead of default triple slash
+    "ifdef-fill-with-blanks": true,        // add this to remove code with blank spaces instead of "//" comments
+    "ifdef-uncomment-prefix": "// #code "  // add this to uncomment code starting with "// #code "
+};
+
+smartMerge = mergeWithRules({
+    
+    module: {
+        rules: 'prepend'
+    }
+    
+});
+
+module.exports = (env) => {
+    webPackConfig = smartMerge(common(env), {
+        module: {
+            rules: [
+                {
+                    test: /\.js?$/, 
+                    exclude: /node_modules/, 
+                    use: [
+                        { loader: "ifdef-loader", options: ifdefLoaderOptions } 
+                    ]
+                }
+            ]
+        },
+        optimization: {
+            minimize: false
+        }
+    });
+    
+    return webPackConfig;
+};
